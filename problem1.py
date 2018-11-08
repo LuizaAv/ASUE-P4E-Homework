496f9e962211f9ef84090df98b2ccff7209cfa2a
Luiza <44584769+LuizaAv@users.noreply.github.com>
2018-11-08 15:50:57 +0000

    Homework2

---
+++ problem1.py
@@ -0,0 +1,33 @@
+print("Question description:Hello there. You must guess whether the number even or odd? You can guess through solving this short problem: x+15*8=1200/5 ;)")
+
+num = int(input("Enter an integer:"))
+if (num % 2) == 0:
+    print("number is even:")
+else:
+    print("number is odd:")
+
+if (num % 2) == 0:
+    print("You are right (Y)")
+else:
+    print("Oops! Wrong answer:(")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
